aeddc6bc9603590e8151d18b92011adf96ce8bc5
1ambda <1amb4a@gmail.com>
2015-08-09 08:51:55 +0900

    use annotation-based AOP

--- toby-spring/src/main/java/com/github/lambda/service/TransactionAspect.java
+++ toby-spring/src/main/java/com/github/lambda/service/TransactionAspect.java
@@ -1,23 +1,33 @@
 package com.github.lambda.service;
 
 
-//@Aspect
-public class TransactionAspect {
-//    @Autowired
-//    PlatformTransactionManager manager;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-//    @Around("execution (* com.github.lambda.UserService.upgradeLevels(..))")
-//    public Object transactAround(ProceedingJoinPoint joinPoint) throws Throwable {
-//
-//        TransactionStatus status = manager.getTransaction(new DefaultTransactionDefinition());
-//
-//        try {
-//            Object result = joinPoint.proceed();
-//            manager.commit(status);
-//            return result;
-//        } catch (RuntimeException e) {
-//            manager.rollback(status);
-//            throw e;
-//        }
-//    }
+@Aspect
+@Component
+public class TransactionAspect {
+    @Autowired
+    PlatformTransactionManager manager;
+
+    @Around("execution (* *..*.upgradeLevels(..))")
+    public Object transactAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        TransactionStatus status = manager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            Object result = joinPoint.proceed();
+            manager.commit(status);
+            return result;
+        } catch (RuntimeException e) {
+            manager.rollback(status);
+            throw e;
+        }
+    }
 }
